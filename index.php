b5bf4e963050ac4cf77ad5ed6070219c6b4ff92e
Rahul Kumar Jha <kumarjhar393@gmail.com>
2022-08-17 15:28:48 +0530

    Modal and form
    Modal Created with a fixed hover button

--- index.php
+++ index.php
@@ -1,34 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-  <title>FlexStart Bootstrap Template - Index</title>
-  <meta content="" name="description">
-
-  <meta content="" name="keywords">
-
-  <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
-
-</head>
+<?php include_once("php/head.php");
+?>
 
 <body>
 
@@ -75,6 +49,8 @@
     </div>
   </header><!-- End Header -->
 
+
+
   <!-- ======= Hero Section ======= -->
   <section id="hero" class="hero d-flex align-items-center">
 
@@ -90,7 +66,7 @@
                 <i class="bi bi-arrow-right"></i>
               </a>
 
-              <a href="#about" class="btn-call-Us scrollto d-inline-flex align-items-center justify-content-center align-self-center">
+              <a href="#about" class="btn-call-Us  scrollto d-inline-flex align-items-center justify-content-center align-self-center">
                 <span>Register Here</span>
                 <i class="bi bi-arrow-right"></i>
               </a>
@@ -1218,107 +1194,80 @@
 
   </main><!-- End #main -->
 
-  <!-- ======= Footer ======= -->
-  <footer id="footer" class="footer">
+  <!-- Footer -->
+  <?php include_once("php/footer.php") ?>
+  <!-- Footer end -->
+  <!-- Vendor Javascript -->
+  <?php include_once("php/jsFooter"); ?>
 
-    <div class="footer-newsletter">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-lg-12 text-center">
-            <h4>Our Newsletter</h4>
-            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
-          </div>
-          <div class="col-lg-6">
-            <form action="" method="post">
-              <input type="email" name="email"><input type="submit" value="Subscribe">
-            </form>
-          </div>
+  <!-- Side button -->
+  <button id="get-free-quote" style="position:fixed;top:70%;right:-60px;transform: rotate(-90deg);z-index:100;" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+    GET FREE QUOTE </button>
+  <!--Side Button End-->
+
+  <!-- Modal -->
+  <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="staticBackdropLabel">Become a member</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
-      </div>
-    </div>
+        <div class="modal-body">
+          <form action="" method="post">
+            <div class="form-group">
+              <div class="row mb-3">
+                <div class="col">
+                  <label for="">Enter your name</label>
+                  <input class="form-control" type="text" placeholder="Name">
+                </div>
+              </div>
+              <div class="row mb-3">
+                <div class="col"><label for="">Enrollment Number <span style="color:red;">*</span></label>
+                  <input class="form-control" type="text" placeholder="0177CS161129 ">
+                </div>
+                <div class="col">
+                  <label for="">Phone <span style="color:red;">*</span></label>
+                  <input class="form-control" type="number" placeholder="eg. +91- 9953100507" >
+                </div>
+              </div>
+              <div class="row mb-3">
+                <div class="col">
+                  <label for="">Email Address <span style="color:red;">*</span></label>
+                  <input class="form-control" type="email" placeholder="eg. xyz@example.com">
+                </div>
+              </div>
+              <div class="row mb-3">
+                <div class="col">
+                  <label for="">-Select your year- <span style="color:red;">*</span></label>
+                  <select class="form-select" aria-label="Default select example">
+                    <option selected="1">1th Year</option>
+                    <option value="2">2th Year</option>
+                    <option value="3">3th Year</option>
+                    <option value="4">4th Year</option>
+                
+                  </select>
+                </div>
+              </div>
 
-    <div class="footer-top">
-      <div class="container">
-        <div class="row gy-4">
-          <div class="col-lg-5 col-md-12 footer-info">
-            <a href="index.html" class="logo d-flex align-items-center">
-              <img src="assets/img/logo.png" alt="">
-              <span>FlexStart</span>
-            </a>
-            <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus.</p>
-            <div class="social-links mt-3">
-              <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-              <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-              <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-              <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+              <div class="row mb-3">
+                <div class="col">
+                <label for="">Enter your skills <span style="color:red;">*</span></label>
+
+                  <textarea style="overflow: auto; min-height:136px;resize:none;" class="form-control" placeholder="Programming, Dancing, singing, painting etc."></textarea>
+                </div>
+              </div>
+              <button type="submit" class="btn btn-primary">SUBMIT</button>
             </div>
-          </div>
-
-          <div class="col-lg-2 col-6 footer-links">
-            <h4>Useful Links</h4>
-            <ul>
-              <li><i class="bi bi-chevron-right"></i> <a href="#">Home</a></li>
-              <li><i class="bi bi-chevron-right"></i> <a href="#">About us</a></li>
-              <li><i class="bi bi-chevron-right"></i> <a href="#">Services</a></li>
-              <li><i class="bi bi-chevron-right"></i> <a href="#">Terms of service</a></li>
-              <li><i class="bi bi-chevron-right"></i> <a href="#">Privacy policy</a></li>
-            </ul>
-          </div>
-
-          <div class="col-lg-2 col-6 footer-links">
-            <h4>Our Services</h4>
-            <ul>
-              <li><i class="bi bi-chevron-right"></i> <a href="#">Web Design</a></li>
-              <li><i class="bi bi-chevron-right"></i> <a href="#">Web Development</a></li>
-              <li><i class="bi bi-chevron-right"></i> <a href="#">Product Management</a></li>
-              <li><i class="bi bi-chevron-right"></i> <a href="#">Marketing</a></li>
-              <li><i class="bi bi-chevron-right"></i> <a href="#">Graphic Design</a></li>
-            </ul>
-          </div>
-
-          <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
-            <h4>Contact Us</h4>
-            <p>
-              A108 Adam Street <br>
-              New York, NY 535022<br>
-              United States <br><br>
-              <strong>Phone:</strong> +1 5589 55488 55<br>
-              <strong>Email:</strong> info@example.com<br>
-            </p>
-
-          </div>
-
+          </form>
+        </div>
+        <div class="modal-footer">
+          <h5 style="color:green;"> Success Text</h5>
+          <h5 style="color:red;"> Faliure Text</h5>
         </div>
       </div>
     </div>
-
-    <div class="container">
-      <div class="copyright">
-        &copy; Copyright <strong><span>FlexStart</span></strong>. All Rights Reserved
-      </div>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/flexstart-bootstrap-startup-template/ -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-      </div>
-    </div>
-  </footer><!-- End Footer -->
-
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
-  <!-- Vendor JS Files -->
-  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
-
-  <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
+  </div>
 
 </body>
 
