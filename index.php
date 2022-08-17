9705b090a2af1121e2e2fbba8ee6896ff63c1f40
Rahul Kumar Jha <kumarjhar393@gmail.com>
2022-08-17 15:39:08 +0530

    Modulation Done
    Each page template prepared

--- index.php
+++ index.php
@@ -1,53 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<?php include_once("php/head.php");
-?>
+<?php include_once("php/head.php");?>
 
 <body>
 
-  <!-- ======= Header ======= -->
-  <header id="header" class="header fixed-top">
-    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
-
-      <a href="index.html" class="logo d-flex align-items-center">
-        <img src="assets/img/logo.png" alt="">
-        <span>Dynamic Drillers</span>
-      </a>
-
-      <nav id="navbar" class="navbar">
-        <ul>
-          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-          <li><a class="nav-link scrollto" href="#about">About</a></li>
-          <li><a class="nav-link scrollto" href="#services">Services</a></li>
-          <li><a class="nav-link scrollto" href="#portfolio">Portfolio</a></li>
-          <li><a class="nav-link scrollto" href="#team">Team</a></li>
-          <li><a href="blog.html">Blog</a></li>
-          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul>
-          </li>
-          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-          <li><a class="getstarted scrollto" href="#about">Get Started</a></li>
-        </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
-      </nav><!-- .navbar -->
-
-    </div>
-  </header><!-- End Header -->
+<!-- Nav -->
+<?php include_once("php/nav.php");?>
+<!-- Nav End -->
 
 
 
@@ -1198,7 +1158,7 @@
   <?php include_once("php/footer.php") ?>
   <!-- Footer end -->
   <!-- Vendor Javascript -->
-  <?php include_once("php/jsFooter"); ?>
+  <?php include_once("php/jsFooter.php"); ?>
 
   <!-- Side button -->
   <button id="get-free-quote" style="position:fixed;top:70%;right:-60px;transform: rotate(-90deg);z-index:100;" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
